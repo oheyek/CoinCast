d7baab05139486359762670e671ccc3f60f08110
oheyek <oheyek@proton.me>
2026-01-05 16:58:05 +0100

    feat: cache solutions to change frequency of api exceed

--- src/fetch_crypto.py
+++ src/fetch_crypto.py
@@ -1,6 +1,10 @@
 import requests
+from cachetools import TTLCache, cached
+
+cache = TTLCache(maxsize=15, ttl=300)
 
 
+@cached(cache)
 def get_current_price(cryptocurrency):
     response = requests.get(
         f"https://api.coingecko.com/api/v3/simple/price?ids={cryptocurrency}&vs_currencies=usd"
@@ -14,17 +18,3 @@ def get_current_price(cryptocurrency):
 # response = requests.get(
 #     "https://api.coingecko.com/api/v3/coins/bitcoin/market_chart?vs_currency=usd&days=30"
 # )
-# print(response.json())
-#
-# response = requests.get("https://api.coingecko.com/api/v3/coins/list")
-# print(response.json())
-#
-# get_current_price("bitcoin")
-# get_current_price("ethereum")
-# get_current_price("tether")
-# get_current_price("binancecoin")
-# get_current_price("solana")
-# get_current_price("ripple")
-# get_current_price("cardano")
-# get_current_price("dogecoin")
-get_current_price("polkadot")
